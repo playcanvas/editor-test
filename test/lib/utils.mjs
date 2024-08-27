689cc5446ba2dc99f0d30899c1aef7c9c31425fb
kpal <kpal81xd@gmail.com>
2024-08-27 15:32:13 +0100

    Added types to poll

--- test/lib/utils.mjs
+++ test/lib/utils.mjs
@@ -1,3 +1,8 @@
+/**
+ * @param {function(any): any} fn - The function to poll.
+ * @param {*} interval - The interval to poll the function.
+ * @returns {Promise<any>} - The result of the function.
+ */
 export const poll = (fn, interval = 500) => {
     return new Promise((resolve, reject) => {
         const int = setInterval(async () => {
