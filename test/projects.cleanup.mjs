21f4aa777d22bdd6cdd2f55c42c65ea2609508d6
kpal <kpal81xd@gmail.com>
2024-08-28 17:01:16 +0100

    refactor

--- test/projects.cleanup.mjs
+++ test/projects.cleanup.mjs
@@ -8,9 +8,11 @@ const USER = 'kpal';
 cleanup('cleanup lone project forks', async ({ page }) => {
     await page.goto(`https://${HOST}/editor`);
     await page.evaluate(initInterface);
+
     const user = await page.evaluate(name => wi.getUser(name), USER);
     const projects = await page.evaluate(userId => wi.getProjects(userId), user.id);
     const forked = projects.filter(project => /FORK$/.test(project.name));
     await Promise.all(forked.map(project => page.evaluate(projectId => wi.deleteProject(projectId), project.id)));
+
     page.close();
 });
