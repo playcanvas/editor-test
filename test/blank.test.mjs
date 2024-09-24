455c9e8b2734bea4d876b175b02c96c52d975585
kpal <kpal81xd@gmail.com>
2024-09-24 13:38:58 +0100

    added code to test default settings for assets tasks

--- test/blank.test.mjs
+++ test/blank.test.mjs
@@ -10,10 +10,25 @@ const OUT_PATH = 'out';
 
 middleware(test);
 
+test.describe.configure({
+    mode: 'serial'
+});
+
+/**
+ * @param {import('@playwright/test').Page} page - The page to search in.
+ * @param {string} name - The name of the setting to find.
+ * @returns {import('@playwright/test').Locator} - The div containing the setting.
+ */
+const getSetting = (page, name) => {
+    return page.locator('div').filter({ hasText: new RegExp(`^${name}$`) }).locator('div');
+};
+
 const projectPath = `${OUT_PATH}/blank`;
 test('create > fork > delete forked > goto editor > goto launcher > delete', async ({ page }) => {
     await fs.promises.mkdir(projectPath, { recursive: true });
     let projectId = '';
+
+    // create
     expect(await capture({
         page,
         outPath: `${projectPath}/create`,
@@ -28,6 +43,7 @@ test('create > fork > delete forked > goto editor > goto launcher > delete', asy
         }
     })).toStrictEqual([]);
 
+    // fork > delete forked
     expect(await capture({
         page,
         outPath: `${projectPath}/fork`,
@@ -45,6 +61,7 @@ test('create > fork > delete forked > goto editor > goto launcher > delete', asy
         }
     })).toStrictEqual([]);
 
+    // goto editor (project)
     const projectUrl = editorProjectUrl(projectId);
     expect(await capture({
         page,
@@ -58,6 +75,7 @@ test('create > fork > delete forked > goto editor > goto launcher > delete', asy
     await page.waitForURL('**/scene/**');
     const sceneId = /scene\/(\d+)/.exec(page.url())[1];
 
+    // goto editor (scene)
     const scenePath = `${projectPath}/${sceneId}`;
     await fs.promises.mkdir(scenePath, { recursive: true });
     const sceneUrl = editorSceneUrl(sceneId);
@@ -70,6 +88,7 @@ test('create > fork > delete forked > goto editor > goto launcher > delete', asy
         }
     })).toStrictEqual([]);
 
+    // goto launcher
     const sceneLaunchUrl = launchSceneUrl(sceneId);
     expect(await capture({
         page,
@@ -80,6 +99,7 @@ test('create > fork > delete forked > goto editor > goto launcher > delete', asy
         }
     })).toStrictEqual([]);
 
+    // delete
     expect(await capture({
         page,
         outPath: `${projectPath}/delete`,
@@ -91,4 +111,36 @@ test('create > fork > delete forked > goto editor > goto launcher > delete', asy
             await page.locator('input[type="submit"]').click();
         }
     })).toStrictEqual([]);
+});
+
+test('check default settings', async ({ page }) => {
+    await fs.promises.mkdir(projectPath, { recursive: true });
+
+    // create
+    await page.goto(`https://${HOST}`);
+    await page.getByText('NEW', { exact: true }).click();
+    await page.getByText('Accept All Cookies', { exact: true }).click();
+    await page.getByRole('button', { name: 'CREATE' }).click();
+    await page.waitForURL('**/project/**');
+    const projectId = /project\/(\d+)/.exec(page.url())[1];
+
+    // goto editor
+    const projectUrl = editorProjectUrl(projectId);
+    await page.goto(projectUrl, { waitUntil: 'networkidle' });
+    await page.waitForURL('**/scene/**');
+
+    // open settings
+    await page.getByRole('button', { name: '' }).click();
+
+    // open asset tasks
+    await page.getByText('ASSET TASKS', { exact: true }).click();
+    expect(await getSetting(page, 'Convert to GLB').getAttribute('class')).toContain('pcui-boolean-input-ticked');
+    expect(await getSetting(page, 'Create FBX Folder').getAttribute('class')).toContain('pcui-boolean-input-ticked');
+
+    // delete
+    await page.goto(`https://${HOST}/project/${projectId}`, { waitUntil: 'networkidle' });
+    await page.getByRole('link', { name: ' SETTINGS' }).click();
+    await page.getByRole('button', { name: 'DELETE' }).click();
+    await page.getByPlaceholder('type here').fill('Blank Project');
+    await page.locator('input[type="submit"]').click();
 });
