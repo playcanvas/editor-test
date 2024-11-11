df1847a412ba9411566e54ce8c9b21094e5995a7
kpal <kpal81xd@gmail.com>
2024-11-11 16:58:46 +0000

    Cleaned up project test

--- test/projects.test.mjs
+++ test/projects.test.mjs
@@ -2,10 +2,8 @@ import fs from 'fs';
 
 import { test, expect } from '@playwright/test';
 
-import { capture } from '../lib/capture.mjs';
-import { downloadProject, publishProject, visitEditorScene, visitLauncher } from '../lib/common.mjs';
+import { downloadProject, publishProject, visitEditor, visitEditorScene, visitLauncher } from '../lib/common.mjs';
 import { middleware } from '../lib/middleware.mjs';
-import { editorProjectUrl } from '../lib/url.mjs';
 
 const OUT_PATH = 'out';
 const PROJECTS = fs.existsSync('cache/projects.json') ? JSON.parse(fs.readFileSync('cache/projects.json', 'utf8')) : [];
@@ -19,17 +17,9 @@ test.describe.configure({
 PROJECTS.forEach((project) => {
     test.describe(`${project.name} (${project.id})`, () => {
         const projectPath = `${OUT_PATH}/${project.id}`;
-        const projectUrl = editorProjectUrl(project.id);
         test('goto editor (project id)', async ({ page }) => {
             await fs.promises.mkdir(projectPath, { recursive: true });
-            expect(await capture({
-                page,
-                outPath: `${projectPath}/editor`,
-                fn: async () => {
-                    await page.goto(projectUrl, { waitUntil: 'networkidle' });
-                    await page.screenshot({ path: `${projectPath}/editor.png` });
-                }
-            }));
+            expect(await visitEditor(page, `${projectPath}/editor`, project.id)).toStrictEqual([]);
         });
 
         project.scenes.forEach((sceneId) => {
@@ -44,15 +34,11 @@ PROJECTS.forEach((project) => {
                 expect(await visitLauncher(page, `${scenePath}/launcher`, sceneId)).toStrictEqual([]);
             });
 
-            test('download', async ({ page }) => {
+            test('download > publish > goto app > delete app', async ({ page }) => {
                 await fs.promises.mkdir(scenePath, { recursive: true });
 
                 // download
                 expect(await downloadProject(page, `${scenePath}/download`, sceneId)).toStrictEqual([]);
-            });
-
-            test('publish > goto app > delete app', async ({ page }) => {
-                await fs.promises.mkdir(scenePath, { recursive: true });
 
                 // publish
                 expect(await publishProject(page, `${scenePath}/publish`, sceneId)).toStrictEqual([]);
