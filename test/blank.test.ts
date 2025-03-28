7c9d2121ab77dbb9b0657962bed1536c4e93c675
kpal <kpal81xd@gmail.com>
2025-03-28 16:49:36 +0000

    Updated blank test to separate tests out

--- test/blank.test.ts
+++ test/blank.test.ts
@@ -1,8 +1,17 @@
 import * as fs from 'fs';
 
-import { expect, test } from '@playwright/test';
+import { expect, test, type Page } from '@playwright/test';
 
-import { createProject, deleteProject, downloadProject, getSetting, publishProject, visitEditor, visitEditorScene, visitLauncher } from '../lib/common';
+import {
+    createProject,
+    deleteProject,
+    downloadProject,
+    getSetting,
+    publishProject,
+    visitEditor,
+    visitEditorScene,
+    visitLauncher
+} from '../lib/common';
 import { middleware } from '../lib/middleware';
 import { id } from '../lib/utils';
 
@@ -17,97 +26,154 @@ test.describe.configure({
 
 const next = id();
 
-test('create > fork > delete forked > goto editor > goto launcher > delete', async ({ page }) => {
+test.describe('create/delete', () => {
     const projectPath = `${OUT_PATH}/${next()}`;
-    await fs.promises.mkdir(projectPath, { recursive: true });
+    let projectId: number;
+    let forkedProjectId: number;
+    let sceneId: number;
+    let page: Page;
 
-    // create
-    const {
-        errors: createErrors,
-        projectId
-    } = await createProject(page, `${projectPath}/create`, PROJECT_NAME);
-    expect(createErrors).toStrictEqual([]);
+    test.describe.configure({
+        mode: 'serial'
+    });
 
-    // fork > delete forked
-    const {
-        errors: forkErrors,
-        projectId: forkedProjectId
-    } = await createProject(page, `${projectPath}/fork-create`, `${PROJECT_NAME} FORK`, projectId);
-    expect(forkErrors).toStrictEqual([]);
-    expect(await deleteProject(page, `${projectPath}/fork-delete`, forkedProjectId)).toStrictEqual([]);
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+        await fs.promises.mkdir(projectPath, { recursive: true });
+    });
 
-    // goto editor (project)
-    const {
-        errors: editorErrors,
-        sceneId
-    } = await visitEditor(page, `${projectPath}/editor`, projectId);
-    expect(editorErrors).toStrictEqual([]);
+    test.afterAll(async () => {
+        await page.close();
+    });
 
-    // goto editor (scene)
-    const scenePath = `${projectPath}/${sceneId}`;
-    await fs.promises.mkdir(scenePath, { recursive: true });
-    expect(await visitEditorScene(page, `${scenePath}/editor`, sceneId)).toStrictEqual([]);
+    test('create project', async () => {
+        const res = await createProject(page, `${projectPath}/create`, PROJECT_NAME);
+        expect(res.errors).toStrictEqual([]);
+        expect(res.projectId).toBeDefined();
+        projectId = res.projectId;
+    });
 
-    // goto launcher
-    expect(await visitLauncher(page, `${projectPath}/launcher`, sceneId)).toStrictEqual([]);
+    test('fork project', async () => {
+        const res = await createProject(page, `${projectPath}/fork-create`, `${PROJECT_NAME} FORK`, projectId);
+        expect(res.errors).toStrictEqual([]);
+        expect(res.projectId).toBeDefined();
+        forkedProjectId = res.projectId;
+    });
 
-    // delete
-    expect(await deleteProject(page, `${projectPath}/delete`, projectId)).toStrictEqual([]);
+    test('delete forked project', async () => {
+        expect(await deleteProject(page, `${projectPath}/fork-delete`, forkedProjectId)).toStrictEqual([]);
+    });
+
+    test('goto editor (project)', async () => {
+        const res = await visitEditor(page, `${projectPath}/editor`, projectId);
+        expect(res.errors).toStrictEqual([]);
+        expect(res.sceneId).toBeDefined();
+        sceneId = res.sceneId;
+    });
+
+    test('goto editor (scene)', async () => {
+        const scenePath = `${projectPath}/${sceneId}`;
+        await fs.promises.mkdir(scenePath, { recursive: true });
+        expect(await visitEditorScene(page, `${scenePath}/editor`, sceneId)).toStrictEqual([]);
+    });
+
+    test('goto launcher', async () => {
+        expect(await visitLauncher(page, `${projectPath}/launcher`, sceneId)).toStrictEqual([]);
+    });
+
+    test('delete project', async () => {
+        expect(await deleteProject(page, `${projectPath}/delete`, projectId)).toStrictEqual([]);
+    });
 });
 
-test('create > goto editor > download > publish > goto app > delete app > delete', async ({ page }) => {
+test.describe('publish/download', () => {
     const projectPath = `${OUT_PATH}/${next()}`;
-    await fs.promises.mkdir(projectPath, { recursive: true });
+    let projectId: number;
+    let sceneId: number;
+    let page: Page;
 
-    // create
-    const {
-        errors: createErrors,
-        projectId
-    } = await createProject(page, `${projectPath}/create`, PROJECT_NAME);
-    expect(createErrors).toStrictEqual([]);
+    test.describe.configure({
+        mode: 'serial'
+    });
 
-    // goto editor (project)
-    const {
-        errors: editorErrors,
-        sceneId
-    } = await visitEditor(page, `${projectPath}/editor`, projectId);
-    expect(editorErrors).toStrictEqual([]);
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+        await fs.promises.mkdir(projectPath, { recursive: true });
+    });
 
-    // download
-    expect(await downloadProject(page, `${projectPath}/download`, sceneId)).toStrictEqual([]);
+    test.afterAll(async () => {
+        await page.close();
+    });
 
-    // publish
-    expect(await publishProject(page, `${projectPath}/publish`, sceneId)).toStrictEqual([]);
+    test('create project', async () => {
+        const res = await createProject(page, `${projectPath}/create`, PROJECT_NAME);
+        expect(res.errors).toStrictEqual([]);
+        expect(res.projectId).toBeDefined();
+        projectId = res.projectId;
+    });
 
-    // delete
-    expect(await deleteProject(page, `${projectPath}/delete`, projectId)).toStrictEqual([]);
+    test('goto editor (project)', async () => {
+        const res = await visitEditor(page, `${projectPath}/editor`, projectId);
+        expect(res.errors).toStrictEqual([]);
+        expect(res.sceneId).toBeDefined();
+        sceneId = res.sceneId;
+    });
+
+    test('download app', async () => {
+        expect(await downloadProject(page, `${projectPath}/download`, sceneId)).toStrictEqual([]);
+    });
+
+    test('publish app', async () => {
+        expect(await publishProject(page, `${projectPath}/publish`, sceneId)).toStrictEqual([]);
+    });
+
+    test('delete project', async () => {
+        expect(await deleteProject(page, `${projectPath}/delete`, projectId)).toStrictEqual([]);
+    });
 });
 
-test('create > goto editor > check default settings > delete', async ({ page }) => {
+test.describe('settings', () => {
     const projectPath = `${OUT_PATH}/${next()}`;
-    await fs.promises.mkdir(projectPath, { recursive: true });
+    let projectId: number;
+    let page: Page;
 
-    // create
-    const {
-        errors: createErrors,
-        projectId
-    } = await createProject(page, `${projectPath}/create`, PROJECT_NAME);
-    expect(createErrors).toStrictEqual([]);
+    test.describe.configure({
+        mode: 'serial'
+    });
 
-    // goto editor
-    const {
-        errors: editorErrors
-    } = await visitEditor(page, `${projectPath}/editor`, projectId);
-    expect(editorErrors).toStrictEqual([]);
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+        await fs.promises.mkdir(projectPath, { recursive: true });
+    });
 
-    // open settings
-    await page.getByRole('button', { name: '' }).click();
+    test.afterAll(async () => {
+        await page.close();
+    });
 
-    // open asset tasks
-    await page.getByText('ASSET TASKS', { exact: true }).click();
-    expect(await getSetting(page, 'Convert to GLB').getAttribute('class')).toContain('pcui-boolean-input-ticked');
-    expect(await getSetting(page, 'Create FBX Folder').getAttribute('class')).toContain('pcui-boolean-input-ticked');
+    test('create project', async () => {
+        const res = await createProject(page, `${projectPath}/create`, PROJECT_NAME);
+        expect(res.errors).toStrictEqual([]);
+        expect(res.projectId).toBeDefined();
+        projectId = res.projectId;
+    });
 
-    // delete
-    expect(await deleteProject(page, `${projectPath}/delete`, projectId)).toStrictEqual([]);
+    test('goto editor (project)', async () => {
+        const res = await visitEditor(page, `${projectPath}/editor`, projectId);
+        expect(res.errors).toStrictEqual([]);
+        expect(res.sceneId).toBeDefined();
+    });
+
+    test('open settings', async () => {
+        await page.getByRole('button', { name: '' }).click();
+    });
+
+    test('check asset tasks', async () => {
+        await page.getByText('ASSET TASKS', { exact: true }).click();
+        expect(await getSetting(page, 'Convert to GLB').getAttribute('class')).toContain('pcui-boolean-input-ticked');
+        expect(await getSetting(page, 'Create FBX Folder').getAttribute('class')).toContain('pcui-boolean-input-ticked');
+    });
+
+    test('delete project', async () => {
+        expect(await deleteProject(page, `${projectPath}/delete`, projectId)).toStrictEqual([]);
+    });
 });
