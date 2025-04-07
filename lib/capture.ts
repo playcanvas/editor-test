86b83176cfec60728eaa5884efc94c1b3e295a45
kpal <kpal81xd@gmail.com>
2025-04-07 16:42:47 +0100

    Removed catching pageerror

--- lib/capture.ts
+++ lib/capture.ts
@@ -19,8 +19,7 @@ export const capture = async ({ page, outPath, callback }: CaptureOptions): Prom
     const errors: string[] = [];
 
     await fs.promises.writeFile(`${outPath}.console.log`, '');
-    await fs.promises.writeFile(`${outPath}.pageerror.log`, '');
-    await fs.promises.writeFile(`${outPath}.request.log`, '');
+    await fs.promises.writeFile(`${outPath}.network.log`, '');
 
     const onConsole = (msg: ConsoleMessage) => {
         if (msg.type() === 'error') {
@@ -29,23 +28,17 @@ export const capture = async ({ page, outPath, callback }: CaptureOptions): Prom
         const msgStr = `[${msg.type()}] ${msg.text()}`;
         fs.promises.appendFile(`${outPath}.console.log`, `${msgStr}\n`);
     };
-    const onPageError = (error: Error) => {
-        const msgStr = `[pageerror] ${error.message}`;
-        fs.promises.appendFile(`${outPath}.pageerror.log`, `${msgStr}\n`);
-    };
     const onResponse = (response: Response) => {
         const msgStr = `[${response.status()}] ${response.url()}`;
-        fs.promises.appendFile(`${outPath}.request.log`, `${msgStr}\n`);
+        fs.promises.appendFile(`${outPath}.network.log`, `${msgStr}\n`);
     };
 
     page.on('console', onConsole);
-    page.on('pageerror', onPageError);
     page.on('response', onResponse);
 
     await callback(errors);
 
     page.removeListener('console', onConsole);
-    page.removeListener('pageerror', onPageError);
     page.removeListener('response', onResponse);
 
     return errors;
