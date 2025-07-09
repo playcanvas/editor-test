e31885ef209351a3c58fa35c0c90826c04f5ed64
kpal <kpal81xd@gmail.com>
2025-07-09 14:44:01 +0100

    Cleanup Playwright configuration by removing commented code and unnecessary comments

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -3,39 +3,19 @@ import { defineConfig, devices } from '@playwright/test';
 import { AUTH_STATE } from './lib/config';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-    /* Timeout for each test */
     timeout: 2 * 60 * 1000,
-    /* Directory to search for tests */
     testDir: './test',
-    /* Run tests in files in parallel */
-    fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    fullyParallel: false, // FIXME: Enable once account per worker is implemented
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    workers: 1, // FIXME: Enable once account per worker is implemented
+    reporter: process.env.CI ? 'list' : 'html',
     use: {
-        /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://127.0.0.1:3000',
-
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry'
     },
-
-    /* Configure projects for major browsers */
     projects: [
         {
             name: 'auth',
