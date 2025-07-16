febec0610a532b7c06d95c09175a5ec2294c48fd
KPal <48248865+kpal81xd@users.noreply.github.com>
2025-07-16 16:59:14 +0100

    Removed WebInterface (using Editor API) (#9)
    * Update @playcanvas/editor-api to version 1.1.22 and fix jobGet parameter structure
    
    * Replaced deleteApp with API call
    
    * Removed inject interface from delete app
    
    * Refactor deleteProject and deleteAllProjects to use API call for project deletion
    
    * Update createProject to use new API call and upgrade @playcanvas/editor-api to version 1.1.24
    
    * Update createProject to use updated API parameter for project creation
    
    * Update pollJob to use new API call for job status retrieval
    
    * Refactor createProject and deleteAllProjects to use updated API calls for project creation and retrieval
    
    * Remove WebInterface class and refactor importProject to handle file uploads directly
    
    * Refactor importProject to use updated API call for project import
    
    * Refactor importProject to throw errors for file selection and import issues, and wait for job completion using event listener
    
    * Removed all uses of poll
    
    * Refactor importProject to replace custom AJAX function with fetch API for file upload process

--- lib/common.ts
+++ lib/common.ts
@@ -1,39 +1,5 @@
 import { type Page } from '@playwright/test';
 
-import { poll, wait } from './utils';
-import { WebInterface } from './web-interface';
-
-/**
- * Inject the web interface into the page.
- *
- * @param page - The page to inject the interface into.
- */
-export const injectInterface = async (page: Page) => {
-    await page.evaluate(`window.wi = new (${WebInterface.toString()})()`);
-};
-
-/**
- * Polls for a job to complete.
- *
- * @param page - The page to poll.
- * @param jobId - The job id to poll.
- * @returns The job result.
- */
-export const pollJob = async (page: Page, jobId: number) => {
-    const job = await poll(async () => {
-        const job = await page.evaluate(jobId => window.wi.checkJob(jobId), jobId);
-        if (job.status !== 'running') {
-            return job;
-        }
-    });
-    if (job.error) {
-        throw new Error(`Job error: ${job.error}`);
-    } else if (job.status !== 'complete') {
-        throw new Error(`Job status: ${job.status}`);
-    }
-    return job;
-};
-
 /**
  * Create a project. If masterProjectId is provided, the project will be forked from the master project.
  *
@@ -43,32 +9,50 @@ export const pollJob = async (page: Page, jobId: number) => {
  * @returns The data result.
  */
 export const createProject = async (page: Page, projectName: string, masterProjectId?: number) => {
-    await injectInterface(page);
+    return await page.evaluate(async ({ name, fork_from }) => {
+        const res: any = await window.editor.api.globals.rest.projects.projectCreate({
+            name,
+            fork_from
+        }).promisify();
 
-    const create = await page.evaluate(
-        ({ name, id }) => window.wi.createProject(window.config.self.username, name, id),
-        {
-            name: projectName,
-            id: masterProjectId
+        // check if not forked (no job created)
+        if (!fork_from && res.id) {
+            // wait for pipeline job to complete (version control documents)
+            await new Promise<void>((resolve, reject) => {
+                const handle = window.editor.api.globals.messenger.on('message', (name: string, data: any) => {
+                    if (name === 'project.create' && data.project_id === res.id) {
+                        handle.unbind();
+                        if (data.status === 'success') {
+                            resolve();
+                        } else {
+                            reject(new Error(`Project creation failed: ${data.error}`));
+                        }
+                    }
+                });
+            });
+
+            // return project id
+            return res.id;
         }
-    );
-    if (create.error) {
-        throw new Error(`Create error: ${create.error}`);
-    }
-    if (!masterProjectId && create.id) {
-        // FIXME: project creation should be complete after response returned by need to wait
-        // for route to be generated
-        await wait(3000);
+
+        // wait for job to complete
+        const job = await new Promise<any>((resolve) => {
+            const handle = window.editor.api.globals.messenger.on('message', async (name: string, data: any) => {
+                if (name === 'job.update' && data.job.id === res.id) {
+                    handle.unbind();
+                    resolve(await window.editor.api.globals.rest.jobs.jobGet({ jobId: data.job.id }).promisify());
+                }
+            });
+        });
+
+        // check for errors
+        if (job.error) {
+            throw new Error(`Create error: ${job.error}`);
+        }
 
         // return project id
-        return create.id;
-    }
-
-    // FIXME: Poll job completion
-    const job = await pollJob(page, create.id);
-
-    // return project id
-    return job.data?.forked_id ?? 0;
+        return job.data?.forked_id ?? 0;
+    }, { name: projectName, fork_from: masterProjectId });
 };
 
 /**
@@ -79,12 +63,9 @@ export const createProject = async (page: Page, projectName: string, masterProje
  * @returns The errors.
  */
 export const deleteProject = async (page: Page, projectId: number) => {
-    await injectInterface(page);
-
-    const success = await page.evaluate(id => window.wi.deleteProject(id), projectId);
-    if (!success) {
-        throw new Error('Failed to delete project');
-    }
+    await page.evaluate((projectId) => {
+        return window.editor.api.globals.rest.projects.projectDelete({ projectId }).promisify();
+    }, projectId);
 };
 
 /**
@@ -93,17 +74,16 @@ export const deleteProject = async (page: Page, projectId: number) => {
  * @param page - The page.
  */
 export const deleteAllProjects = async (page: Page) => {
-    await injectInterface(page);
-
-    const projects = await page.evaluate(() => window.wi.getProjects(window.config.self.id));
-
+    const projects = await page.evaluate(async () => {
+        const res: any = await window.editor.api.globals.rest.users.userProjects(window.config.self.id, '').promisify();
+        return res.result ?? [];
+    });
     let deletePromise = Promise.resolve();
     for (const project of projects) {
         deletePromise = deletePromise.then(async () => {
-            const success = await page.evaluate(id => window.wi.deleteProject(id), project.id);
-            if (!success) {
-                throw new Error('Failed to delete project');
-            }
+            await page.evaluate((projectId) => {
+                return window.editor.api.globals.rest.projects.projectDelete({ projectId }).promisify();
+            }, project.id);
         });
     }
 
@@ -118,22 +98,124 @@ export const deleteAllProjects = async (page: Page) => {
  * @returns The data result.
  */
 export const importProject = async (page: Page, importPath: string) => {
-    await injectInterface(page);
-
-    // start import
+    // import project
     const fileChooserPromise = page.waitForEvent('filechooser');
-    const importProjectPromise = page.evaluate(() => window.wi.startImport(window.config.self.id));
+    const importProjectPromise = page.evaluate(async () => {
+        const filePicker = document.createElement('input');
+        filePicker.id = 'file-picker';
+        filePicker.type = 'file';
+        filePicker.accept = 'application/zip';
+        filePicker.click();
+        const files = await new Promise<FileList | null>((resolve) => {
+            filePicker.addEventListener('change', () => {
+                resolve(filePicker.files);
+            });
+        });
+        filePicker.remove();
+
+        if (!files || files.length === 0) {
+            throw new Error('No files selected');
+        }
+        const file = files[0];
+
+        const form = new FormData();
+        form.append('file', file);
+
+        // calculate chunk count
+        const chunkSize = 20 * 1024 * 1024;
+        const chunkCount = Math.ceil(file.size / chunkSize);
+
+        // start upload
+        const startRes = await fetch('/api/upload/start-upload', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${window.config.accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                fileName: file.name
+            })
+        });
+        const startJson = await startRes.json();
+
+        // get signed urls
+        const signedRes = await fetch('/api/upload/signed-urls', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${window.config.accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                uploadId: startJson.uploadId,
+                parts: chunkCount,
+                key: startJson.key
+            })
+        });
+        const signedJson = await signedRes.json();
+
+        // upload chunks
+        let chunk = 1;
+        const promises = [];
+        for (let start = 0; start < file.size; start += chunkSize) {
+            const end = Math.min(start + chunkSize, file.size);
+            const blob = file.slice(start, end);
+            const url = signedJson.signedUrls[chunk - 1];
+            promises.push(fetch(url, {
+                method: 'PUT',
+                body: blob,
+                headers: {
+                    'Content-Type': 'application/zip'
+                }
+            }));
+            chunk++;
+        }
+        const uploadRes = await Promise.all(promises);
+
+        // get etags
+        const parts = [];
+        for (let i = 0; i < uploadRes.length; i++) {
+            const res = uploadRes[i];
+            const etag = res.headers.get('ETag') ?? '';
+            const cleanEtag = etag.replace(/^"|"$/g, '');
+            parts.push({ PartNumber: i + 1, ETag: cleanEtag });
+        }
+
+        // complete upload
+        await fetch('/api/upload/complete-upload', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${window.config.accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                uploadId: startJson.uploadId,
+                parts: parts,
+                key: startJson.key
+            })
+        });
+
+        // import project
+        const job: any = await window.editor.api.globals.rest.projects.projectImport({
+            export_url: startJson.key,
+            owner: window.config.self.id
+        }).promisify();
+
+        // wait for job to complete
+        return await new Promise<any>((resolve) => {
+            const handle = window.editor.api.globals.messenger.on('message', async (name: string, data: any) => {
+                if (name === 'job.update' && data.job.id === job.id) {
+                    handle.unbind();
+                    resolve(await window.editor.api.globals.rest.jobs.jobGet({ jobId: data.job.id }).promisify());
+                }
+            });
+        });
+    });
     const fileChooser = await fileChooserPromise;
     await fileChooser.setFiles(importPath);
-    const importProject = await importProjectPromise;
-    if (importProject.error) {
-        throw new Error(`Import error: ${importProject.error}`);
+    const job = await importProjectPromise;
+    if (job.error) {
+        throw new Error(`Import error: ${job.error}`);
     }
-
-    // FIXME: Poll job completion
-    const job = await pollJob(page, importProject.id);
-
-    // return project id
     return job.data?.project_id ?? 0;
 };
 
@@ -174,7 +256,7 @@ export const downloadApp = async (page: Page, sceneId: number): Promise<{ downlo
             const handle = window.editor.api.globals.messenger.on('message', async (name: string, data: any) => {
                 if (name === 'job.update' && data.job.id === job.id) {
                     handle.unbind();
-                    resolve(await window.editor.api.globals.rest.jobs.jobGet(data.job.id).promisify());
+                    resolve(await window.editor.api.globals.rest.jobs.jobGet({ jobId: data.job.id }).promisify());
                 }
             });
         });
@@ -245,10 +327,11 @@ export const publishApp = async (page: Page, sceneId: number): Promise<{ id: num
  * @param appId - The app id.
  */
 export const deleteApp = async (page: Page, appId: number) => {
-    await injectInterface(page);
-
-    const delJob = await page.evaluate(appId => window.wi.deleteApp(appId), appId);
-    if (delJob.error) {
-        throw new Error(`Delete error: ${delJob.error}`);
+    const job = await page.evaluate(async (appId) => {
+        const app: any = await window.editor.api.globals.rest.apps.appDelete(appId).promisify();
+        return app.task ?? { error: 'Job not found' };
+    }, appId);
+    if (job.error) {
+        throw new Error(`Delete error: ${job.error}`);
     }
 };
